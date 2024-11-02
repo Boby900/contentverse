47a2c67f4c0f6e5d83553af2d5852af15443759c
Boby900 <bobytiwari310@gmail.com>
2024-11-02 14:35:54 +0530

    setup the mvc model for the application

---
+++ controllers/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+    getHome: async (req, res) => {
+
+            
+            res.status(201).send('Hello World!')
+
+    }
+}
---
+++ controllers/user.js
@@ -0,0 +1,4 @@
+module.exports = {
+    getUser: async (req, res) => {
+        res.status(200).send('Hello from the user!')
+}}
