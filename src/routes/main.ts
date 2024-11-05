af751dcaa79830b5bceb9be17758fa9a019131c4
Boby900 <bobytiwari310@gmail.com>
2024-11-05 20:23:23 +0530

    commonjs to module

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,12 +1,13 @@
-const express = require('express');
-const mainController = require('../controllers/main')
+import express from 'express';
+import {getHome} from '../controllers/main.ts';
+
 const router = express.Router();
 
-router.get('/', mainController.getHome);
+router.get('/', getHome);
 // router.get("/login", mainController.getLogin);
 // router.post("/login", mainController.postLogin);
 // router.get("/logout", mainController.logout);
 // router.get("/signup", mainController.getSignup);
 // router.post("/signup", mainController.postSignup);
 
-module.exports = router;
+export default router;
