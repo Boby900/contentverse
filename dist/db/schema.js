837f9521d4ab825d97c793162e0e2faf96d03a8c
Boby Tiwari <bobytiwari310@gmail.com>
2024-11-06 14:32:18 +0530

    Bar (#5)
    * some refactoring...

--- dist/db/schema.js
+++
@@ -1,7 +0,0 @@
-import { pgTable, serial, text } from 'drizzle-orm/pg-core';
-export const usersTable = pgTable('users_table', {
-    id: serial('id').primaryKey(),
-    name: text('name').notNull(),
-});
-// export type InsertUser = typeof usersTable.$inferInsert;
-// export type SelectUser = typeof usersTable.$inferSelect;
