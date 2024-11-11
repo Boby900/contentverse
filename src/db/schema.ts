37551eb6fa38164195291f102a704992e64f7323
Boby900 <bobytiwari310@gmail.com>
2024-11-11 12:20:19 +0530

    added new schema for the content table

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,23 +1,31 @@
-import { pgTable, serial, text, integer, timestamp} from 'drizzle-orm/pg-core';
+import { pgTable, serial, text, integer, timestamp } from "drizzle-orm/pg-core";
 import type { InferSelectModel } from "drizzle-orm";
+import { title } from "process";
 export const userTable = pgTable("user", {
-	id: serial("id").primaryKey()
+  id: serial("id").primaryKey(),
 });
 
 export const sessionTable = pgTable("session", {
-	id: text("id").primaryKey(),
-	userId: integer("user_id")
-		.notNull()
-		.references(() => userTable.id),
-	expiresAt: timestamp("expires_at", {
-		withTimezone: true,
-		mode: "date"
-	}).notNull()
+  id: text("id").primaryKey(),
+  userId: integer("user_id")
+    .notNull()
+    .references(() => userTable.id),
+  expiresAt: timestamp("expires_at", {
+    withTimezone: true,
+    mode: "date",
+  }).notNull(),
 });
+export const contentTable = pgTable("content", {
+  id: text("id").primaryKey(),
+  userId: integer("user_id")
+    .notNull()
+    .references(() => userTable.id),
+  title: text("title") 
+});
+//TODO:
+//implement some logic in the auth handler, ref => auth.ts
+//learn more about middleware
+//test the auth in backend only, I guess using the header it's possible.
 
 export type User = InferSelectModel<typeof userTable>;
 export type Session = InferSelectModel<typeof sessionTable>;
-
-
-
-
