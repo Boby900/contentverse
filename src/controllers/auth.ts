af24311de76aa25d8fe5a2ee88ff430461bdf341
Boby Tiwari <bobytiwari310@gmail.com>
2024-11-22 18:56:03 +0530

    Bob (#14)
    * setting up the zod and validating the inputs

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -9,48 +9,72 @@ import {
 } from "@oslojs/encoding";
 import { sha256 } from "@oslojs/crypto/sha2";
 import crypto from "crypto";
+import { z, ZodError } from "zod";
+
+const signupSchema = z.object({
+  email: z.string().email().min(5),
+  password: z.string().min(5),
+});
 
 export const signupHandler = async (req: Request, res: Response) => {
-  const { email, password } = req.body;
-  // Check if user already exists
-  const existingUser = await db
-    .select()
-    .from(userTable)
-    .where(eq(userTable.email, email));
-  if (existingUser.length >= 1) {
-    res.status(404).send({ message: "User already exists" });
-  } else {
-    // // Hash password (implement hashing logic with oslojs or other)
-    const hashedPassword = encodeHexLowerCase(
-      sha256(new TextEncoder().encode(password))
-    );
-    // // Insert user into database
-    const newUser = await db
-      .insert(userTable)
-      .values({ email, password: hashedPassword });
-    res
-      .status(201)
-      .json({ message: "User registered successfully", user: newUser });
+  try {
+    const { email, password } = signupSchema.parse(req.body);
+    // Check if user already exists
+    const existingUser = await db
+      .select()
+      .from(userTable)
+      .where(eq(userTable.email, email));
+    if (existingUser.length >= 1) {
+      res.status(404).send({ message: "User already exists" });
+    } else {
+      // // Hash password (implement hashing logic with oslojs or other)
+      const hashedPassword = encodeHexLowerCase(
+        sha256(new TextEncoder().encode(password))
+      );
+      // // Insert user into database
+      const newUser = await db
+        .insert(userTable)
+        .values({ email, password: hashedPassword });
+      res
+        .status(201)
+        .json({ message: "User registered successfully", user: newUser });
+    }
+  } catch (error) {
+    if (error instanceof ZodError) {
+      res.status(400).json({
+        message: "Validation failed",
+        errors: error.errors, // Detailed validation issues
+      });
+    }
   }
 };
 
 export const loginHandler = async (req: Request, res: Response) => {
-  const { email, password } = req.body;
-  // Find user by email
-  const user = await db
-    .select()
-    .from(userTable)
-    .where(eq(userTable.email, email));
-  const hashedPassword = encodeHexLowerCase(
-    sha256(new TextEncoder().encode(password))
-  );
-  if (user.length < 1 || user[0].password !== hashedPassword) {
-    res.status(401).json({ message: "Invalid email or password" });
-  } else {
-    // Generate session token
-    const token = generateSessionToken();
-    await createSession(token, user[0].id);
-    res.status(200).json({ message: "Logged in successfully", token });
+  try {
+    const { email, password } = signupSchema.parse(req.body);
+    // Find user by email
+    const user = await db
+      .select()
+      .from(userTable)
+      .where(eq(userTable.email, email));
+    const hashedPassword = encodeHexLowerCase(
+      sha256(new TextEncoder().encode(password))
+    );
+    if (user.length < 1 || user[0].password !== hashedPassword) {
+      res.status(401).json({ message: "Invalid email or password" });
+    } else {
+      // Generate session token
+      const token = generateSessionToken();
+      await createSession(token, user[0].id);
+      res.status(200).json({ message: "Logged in successfully", token });
+    }
+  } catch (error) {
+    if (error instanceof ZodError) {
+      res.status(400).json({
+        message: "Validation failed",
+        errors: error.errors, // Detailed validation issues
+      });
+    }
   }
 };
 function generateSessionToken(): string {
--- src/controllers/content.ts
+++ src/controllers/content.ts
@@ -3,21 +3,22 @@ import { db } from "../db/index.js";
 import { contentTable } from "../db/schema.js";
 import { randomUUID } from "crypto";
 import { eq } from "drizzle-orm";
+import { z, ZodError } from "zod";
+
+const contentSchema = z.object({
+  title: z.string().min(5),
+  userId: z.number(),
+});
+
 
 export const createContent = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const { title, userId } = req.body;
+
   try {
-    if (!title || !userId) {
-      res.status(400).json({
-        status: "fail",
-        message: "Title and userId are required",
-      });
-      return;
-    }
+    const { title, userId } = contentSchema.parse(req.body);
     await db.insert(contentTable).values({
       id: randomUUID(), // Generate a unique ID
       title: title,
@@ -28,12 +29,19 @@ export const createContent = async (
       message: "Content created successfully",
     });
   } catch (error: unknown) {
-    console.error(error);
+    if (error instanceof ZodError) {
+     
+      res.status(400).json({
+        status: "fail",
+        errors: error.errors, // Include detailed validation errors
+      });
+    }
     res.status(500).json({
       status: "error",
       message: "Internal server error while creating content",
       error: error,
     });
+    
   }
 };
 
@@ -103,15 +111,13 @@ export const updateContentByID = async (
   next: NextFunction
 ) => {
   try {
+    const validData = contentSchema.partial({
+      userId: true
+    })
     const { id } = req.params;
-    const { title } = req.body;
-    if(!title){
-      res.status(404).json({
-        status: "fail",
-        message:"no title provided"
-      })
-      return
-    }
+    const { title } =  validData.parse(req.body);
+    
+    
     const data = await db
       .update(contentTable)
       .set({"title": title })
@@ -130,13 +136,20 @@ export const updateContentByID = async (
       message: "Content updated successfully",
       data: data,
     });
-  } catch (error) {
-    console.error(error);
+  } catch (error: unknown) {
+    if (error instanceof ZodError) {
+     
+     return res.status(400).json({
+        status: "fail",
+        errors: error.errors, // Include detailed validation errors
+      });
+    }
     res.status(500).json({
       status: "error",
-      message: "Internal server error while fetching content",
+      message: "Internal server error while creating content",
       error: error,
     });
+    
   }
 };
 export const deleteContentByID = async (
