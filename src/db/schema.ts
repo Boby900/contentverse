e3b05d0a0dd98ff52c29e16c8e986301618dedf0
Boby900 <bobytiwari310@gmail.com>
2024-11-27 14:32:14 +0530

    adding mediaUpload function and setting up the schema for that👻

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,10 +1,10 @@
 import { pgTable, serial, text, integer, timestamp } from "drizzle-orm/pg-core";
 import type { InferSelectModel } from "drizzle-orm";
 export const userTable = pgTable("user", {
-  id: serial("id").primaryKey(),  // TODO: use randomevalues from crypto if possible, instead of generic numbers.
-  email: text("email").notNull(),     // For identifying users
-  password: text("password").notNull(),         // For storing hashed passwords
-  createdAt: timestamp("created_at", {         // Optional: track account creation time
+  id: serial("id").primaryKey(),
+  email: text("email").notNull(), 
+  password: text("password").notNull(), 
+  createdAt: timestamp("created_at", {
     withTimezone: true,
     mode: "date",
   }).defaultNow(),
@@ -25,9 +25,19 @@ export const contentTable = pgTable("content", {
   userId: integer("user_id")
     .notNull()
     .references(() => userTable.id),
-  title: text("title") 
+  title: text("title"),
+});
+export const mediaTable = pgTable("media", {
+  id: text("id").primaryKey(),
+  userId: integer("user_id")
+    .notNull()
+    .references(() => userTable.id),
+  pinata_id: text("pinata_id").notNull(),
+  cid: text("cid").notNull(),
+  mime_type: text("mime_type").notNull(),
+  user_pinata_id: text("user_pinata_id").notNull()
 });
 
-
 export type User = InferSelectModel<typeof userTable>;
+export type Media = InferSelectModel<typeof mediaTable>;
 export type Session = InferSelectModel<typeof sessionTable>;
