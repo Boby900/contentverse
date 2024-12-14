1c1dad9ff00fafbf010699184499a930e078767e
boby900 <bobyt2265@gmail.com>
2024-12-14 20:02:22 +0530

    added cascading rules so when user is deleted all the related data to that user should be deleted

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -2,8 +2,8 @@ import { pgTable, serial, text, integer, timestamp } from "drizzle-orm/pg-core";
 import type { InferSelectModel } from "drizzle-orm";
 export const userTable = pgTable("user", {
   id: serial("id").primaryKey(),
-  email: text("email").notNull(), 
-  password: text("password").notNull(), 
+  email: text("email").notNull(),
+  password: text("password").notNull(),
   createdAt: timestamp("created_at", {
     withTimezone: true,
     mode: "date",
@@ -14,7 +14,7 @@ export const sessionTable = pgTable("session", {
   id: text("id").primaryKey(),
   userId: integer("user_id")
     .notNull()
-    .references(() => userTable.id),
+    .references(() => userTable.id, { onDelete: "cascade" }),
   expiresAt: timestamp("expires_at", {
     withTimezone: true,
     mode: "date",
@@ -24,18 +24,18 @@ export const contentTable = pgTable("content", {
   id: text("id").primaryKey(),
   userId: integer("user_id")
     .notNull()
-    .references(() => userTable.id),
+    .references(() => userTable.id, { onDelete: "cascade" }),
   title: text("title"),
 });
 export const mediaTable = pgTable("media", {
   id: text("id").primaryKey(),
   userId: integer("user_id")
     .notNull()
-    .references(() => userTable.id),
+    .references(() => userTable.id, { onDelete: "cascade" }),
   pinata_id: text("pinata_id").notNull(),
   cid: text("cid").notNull(),
   mime_type: text("mime_type").notNull(),
-  user_pinata_id: text("user_pinata_id").notNull()
+  user_pinata_id: text("user_pinata_id").notNull(),
 });
 
 export type User = InferSelectModel<typeof userTable>;
