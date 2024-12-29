d8f0f84ff026f5940798650da38ce717e00ac948
boby900 <bobyt2265@gmail.com>
2024-12-29 08:53:27 +0530

    configured the redirectURI

--- src/lib/github.ts
+++ src/lib/github.ts
@@ -1,8 +1,10 @@
-
 import { GitHub } from "arctic";
-
+const redirectUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://contentverse-production.up.railway.app/api/auth/github/login/callback"
+    : "http://localhost:3000/api/auth/github/login/callback";
 export const github = new GitHub(
-    process.env.GITHUB_CLIENT_ID!,
-    process.env.GITHUB_CLIENT_SECRET!,
-    null
+  process.env.GITHUB_CLIENT_ID!,
+  process.env.GITHUB_CLIENT_SECRET!,
+  redirectUrl // Replace with the appropriate URI for your environment
 );
