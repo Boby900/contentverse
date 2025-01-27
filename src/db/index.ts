6d3b93d26f9214d127e2c8ece8b61bd96fa55c45
boby900 <bobyt2265@gmail.com>
2025-01-27 15:18:46 +0530

    ref: refactored the insert collection api endpoint to return the succesful message after submitting the form

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,19 +1,12 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
-import * as schema from "./schema.js"
+import * as schema from "./schema.js";
 import { config } from "dotenv";
 config({ path: ".env" }); // or .env.local
 // uncommenting above lines will cause the error
-const sql = neon(process.env.DATABASE_URL!,{
-    fetchOptions: {
-        signal: AbortSignal.timeout(1000000) // 10 seconds timeout
-      }
-}
-    
-);
-
-
-
-
-
-export const db = drizzle(sql, {schema});
+const sql = neon(process.env.DATABASE_URL!, {
+  fetchOptions: {
+    signal: AbortSignal.timeout(9000000), // 10 seconds timeout
+  },
+});
+export const db = drizzle(sql, { schema });
