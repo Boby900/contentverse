c89d32e2d2ab99bedfb4513559b8b717d2920a59
boby900 <bobyt2265@gmail.com>
2025-07-05 12:37:09 +0530

    refactor: update r2 controller import to use .js extension

--- src/routes/r2.ts
+++ src/routes/r2.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import multer from "multer";
-import { uploadToR2 } from "../controllers/r2";
+import { uploadToR2 } from "../controllers/r2.js";
 
 const router = Router();
 const storage = multer.memoryStorage();
